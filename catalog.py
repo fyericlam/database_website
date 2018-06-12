cb477fc6b6c359f773aa64a32b47cdc027f28f63
fyericlam <brick.ericlam@gmail.com>
2018-06-12 12:15:00 +0800

    Import CRUD modules and create connection to database

--- catalog.py
+++ catalog.py
@@ -1,6 +1,16 @@
 from flask import Flask
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Shop, CatalogItem
+
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///shopCatalog.db',
+                       connect_args={'check_same_thread': False})
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
