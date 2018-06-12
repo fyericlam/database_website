3e496b41e19a5e7fd3c86b99f5aeab0e79b8edcf
fyericlam <brick.ericlam@gmail.com>
2018-06-12 12:04:16 +0800

    Update response function names

--- catalog.py
+++ catalog.py
@@ -5,44 +5,44 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/shops/')
-def showRestaurant():
+def showShops():
     return 'This page shows all my shops'
 
 
 @app.route('/shops/new/')
-def newRestaurant():
+def newShop():
     return 'This page makes new shop'
 
 
 @app.route('/shops/<int:shop_id>/edit/')
-def editRestaurant(shop_id):
+def editShop(shop_id):
     return 'This page edits shop {}'.format(shop_id)
 
 
 @app.route('/shops/<int:shop_id>/delete/')
-def deleteRestaurant(shop_id):
+def deleteShop(shop_id):
     return 'This page deletes shop {}'.format(shop_id)
 
 
 @app.route('/shops/<int:shop_id>/')
 @app.route('/shops/<int:shop_id>/catalog/')
-def showMenu(shop_id):
+def showCatalog(shop_id):
     return 'This page displays catalog for shop {}'.format(shop_id)
 
 
 @app.route('/shops/<int:shop_id>/catalog/new/')
-def newMenuItem(shop_id):
+def newCatalogItem(shop_id):
     return 'This page makes new catalog item for shop {}'.format(
         shop_id)
 
 
 @app.route('/shops/<int:shop_id>/catalog/<int:catalog_id>/edit/')
-def editMenuItem(shop_id, catalog_id):
+def editCatalogItem(shop_id, catalog_id):
     return 'This page edits catalog item {}'.format(catalog_id)
 
 
 @app.route('/shops/<int:shop_id>/catalog/<int:catalog_id>/delete/')
-def deleteMenuItem(shop_id, catalog_id):
+def deleteCatalogItem(shop_id, catalog_id):
     return 'This page deletes catalog item {}'.format(catalog_id)
 
 
